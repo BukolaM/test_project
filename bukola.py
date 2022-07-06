8735d6b524108b8cf75c2b5c1fbd439fcf49bb63
Bukola Ajayi <bukolam.ajayi@gmail.com>
2022-07-06 22:07:04 +0100

    full project

---
+++ bukola.py
@@ -0,0 +1,317 @@
+# '''b=1
+# print(b)
+
+# name='Bukola'
+# print(name)
+# print(name[1])
+
+# name=('Bukola','Tsoin','Margaret')
+# print(name[1:2])
+
+# fruits=('apple','berry','chrry')
+# x=('fruits')
+# print(x)
+
+# print(9+9)'''
+
+# '''x=5
+# y='Bukola'
+# print(str(x) + y)
+
+# a='hELLO WORLD'
+# print(a[1])
+# print(len(a))
+
+
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple)
+
+
+# x=5*10
+# print(x)
+
+
+# x=ax**+bx*+c'''
+
+
+# # vowels = ['a', 'e', 'i', 'o', 'u']
+# # consonant= 2
+# # vowels.append(consonant)
+
+# # vowels.append("tosin")
+
+
+# # print (vowels)
+
+# # print(vowels[0] + str(consonant))
+# # letter = 'The value is '
+# # items = ''
+# # for x in vowels:
+# #     print(x)
+# #     item = letter + str(x)
+# #     items = items + " "+ item
+# #     print(items)
+# #     # print("The value is " + str(x))
+
+# # print (letter)
+
+# from re import A
+
+
+# vowels = ['a', 'e', 'i', 'o', 'u']
+
+# print (vowels)
+
+# books = ["g", "h"]
+
+# g = vowels + books
+
+# # print (g)
+
+# # a= 1
+# # b= 2
+# # c = 3
+
+
+# # x=a**2 + b**3 + c
+
+# ax = 4
+# bx = 4
+# cx = 3
+# t = 2
+# x = ax**t + bx**t + cx * t
+# t = 4
+# # print(x)
+# # x = ax**t + bx**t + cx * t
+# # print(x)
+# # print(g)
+
+# #g.append(x)
+
+#print(g)
+
+
+
+
+# a=['yemi','tunde','kola','gbemi','faruk','yetunde','emma']
+# d=5
+# print(a)
+# a.append(d)
+
+# # for x in a:
+# #     print(x)
+
+
+
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# # consonant= 2
+# # vowels.append(consonant)
+
+# # vowels.append("tosin")
+# # print(vowels)
+# # for c in vowels:
+# #     print(c)
+
+
+
+# numbers= [1, 2, 4, 4, 3, 3, 3, 6, 5]
+# a=numbers
+
+# values= [1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
+# # b=values
+
+# # # a.append(b)
+
+# for c in 'Geeks':
+#     print(c)
+
+
+
+
+
+# values= [1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
+# print(values[3])
+
+# a=('Hello','World')
+# # print(a)
+
+
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# values= [1, 2, 'Geeks', 4, 'For', 6, 'Geeks', ['e','r','rr']  ]
+
+# values.append(vowels)
+
+# # print (values[8])
+
+# iddd = values[7]
+
+
+# for item in values[7]:
+#     print(item)
+
+# awwwww ='e'
+
+# vowels.remove(awwwww)
+
+
+
+countries = ['nigeria', 'usa', 'rusia', 'ghana', 'togo']
+
+# # print (countries[0])
+
+# g=len(countries)
+# print(g)
+
+# for country in countries:
+#     print(country)
+
+
+# if 'nigeria' in countries:
+#     print('found')
+# else:
+#     print ('not_found')
+
+
+
+
+# if 'nigeria' in countries:
+#     print('found')
+
+#     print('Nigeria is a great country')
+
+#     c =1 + 7
+
+#     print(c)
+
+
+
+#     print('eeeeee')
+# else:
+#     print('not found')
+
+
+
+
+# countries = ['nigeria', 'usa', 'rusia', 'ghana', 'togo','united kindom']
+
+# good_countries = ['usa', 'rusia', 'togo']
+
+# bad_countries = []
+
+# for country in countries:
+    
+#     if country not in good_countries:
+#        bad_countries.append(country)
+# print(bad_countries)
+
+
+
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+
+# for fruit in fruits:
+#     newlist.append(fruit)
+# print(newlist)
+
+
+
+
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort()
+# # print(thislist)
+
+# thisset = {"apple", "banana", "cherry"}
+
+# thisset.add("orange")
+
+# print(thisset)
+
+
+# animals= ['dog','cat','rabbit','puppy','chicken', 'snake','lion', 'elephant','monkey','giraffe']
+# wild_animals= ['lion', 'elephant','monkey','giraffe']
+# pets = []
+
+
+# for animal in animals:
+#     if animal not in wild_animals:
+#         print(animal)
+
+# a = " Hello, World!"
+# print(a.lower())
+# print(a.upper())
+# print(a.strip())
+
+# print(len(a))
+
+
+
+# a = 1
+# b = 2
+# c= 5
+# y=a**2 + b**2 + c*1
+# print(y)
+
+# if a<b:
+#     print(c/1)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[-2])
+
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+
+
+
+
+
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
