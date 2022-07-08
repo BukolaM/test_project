3d6b6f910fa716fa752eb44a3cb5f04710388fed
Bukola Ajayi <bukolam.ajayi@gmail.com>
2022-07-08 11:57:22 +0100

    while

--- bukola.py
+++ bukola.py
@@ -73,219 +73,315 @@
 # # c = 3
 
 
-# # x=a**2 + b**3 + c
+# # # x=a**2 + b**3 + c
 
-# ax = 4
-# bx = 4
-# cx = 3
-# t = 2
-# x = ax**t + bx**t + cx * t
-# t = 4
-# # print(x)
+# # ax = 4
+# # bx = 4
+# # cx = 3
+# # t = 2
 # # x = ax**t + bx**t + cx * t
-# # print(x)
-# # print(g)
+# # t = 4
+# # # print(x)
+# # # x = ax**t + bx**t + cx * t
+# # # print(x)
+# # # print(g)
 
-# #g.append(x)
+# # #g.append(x)
 
-#print(g)
-
-
-
-
-# a=['yemi','tunde','kola','gbemi','faruk','yetunde','emma']
-# d=5
-# print(a)
-# a.append(d)
-
-# # for x in a:
-# #     print(x)
+# #print(g)
 
 
 
-# vowels = ['a', 'e', 'i', 'o', 'u']
-# # consonant= 2
-# # vowels.append(consonant)
 
-# # vowels.append("tosin")
-# # print(vowels)
-# # for c in vowels:
+# # a=['yemi','tunde','kola','gbemi','faruk','yetunde','emma']
+# # d=5
+# # print(a)
+# # a.append(d)
+
+# # # for x in a:
+# # #     print(x)
+
+
+
+# # vowels = ['a', 'e', 'i', 'o', 'u']
+# # # consonant= 2
+# # # vowels.append(consonant)
+
+# # # vowels.append("tosin")
+# # # print(vowels)
+# # # for c in vowels:
+# # #     print(c)
+
+
+
+# # numbers= [1, 2, 4, 4, 3, 3, 3, 6, 5]
+# # a=numbers
+
+# # values= [1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
+# # # b=values
+
+# # # # a.append(b)
+
+# # for c in 'Geeks':
 # #     print(c)
 
 
 
-# numbers= [1, 2, 4, 4, 3, 3, 3, 6, 5]
-# a=numbers
-
-# values= [1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
-# # b=values
-
-# # # a.append(b)
-
-# for c in 'Geeks':
-#     print(c)
 
 
+# # values= [1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
+# # print(values[3])
+
+# # a=('Hello','World')
+# # # print(a)
 
 
+# # vowels = ['a', 'e', 'i', 'o', 'u']
+# # values= [1, 2, 'Geeks', 4, 'For', 6, 'Geeks', ['e','r','rr']  ]
 
-# values= [1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
-# print(values[3])
+# # values.append(vowels)
 
-# a=('Hello','World')
-# # print(a)
+# # # print (values[8])
 
-
-# vowels = ['a', 'e', 'i', 'o', 'u']
-# values= [1, 2, 'Geeks', 4, 'For', 6, 'Geeks', ['e','r','rr']  ]
-
-# values.append(vowels)
-
-# # print (values[8])
-
-# iddd = values[7]
+# # iddd = values[7]
 
 
-# for item in values[7]:
-#     print(item)
+# # for item in values[7]:
+# #     print(item)
 
-# awwwww ='e'
+# # awwwww ='e'
 
-# vowels.remove(awwwww)
+# # vowels.remove(awwwww)
 
 
 
-countries = ['nigeria', 'usa', 'rusia', 'ghana', 'togo']
+# countries = ['nigeria', 'usa', 'rusia', 'ghana', 'togo']
 
-# # print (countries[0])
+# # # print (countries[0])
 
-# g=len(countries)
-# print(g)
+# # g=len(countries)
+# # print(g)
 
-# for country in countries:
-#     print(country)
+# # for country in countries:
+# #     print(country)
 
 
-# if 'nigeria' in countries:
-#     print('found')
-# else:
-#     print ('not_found')
+# # if 'nigeria' in countries:
+# #     print('found')
+# # else:
+# #     print ('not_found')
 
 
 
 
-# if 'nigeria' in countries:
-#     print('found')
-
-#     print('Nigeria is a great country')
-
-#     c =1 + 7
-
-#     print(c)
-
-
-
-#     print('eeeeee')
-# else:
-#     print('not found')
 
 
 
 
-# countries = ['nigeria', 'usa', 'rusia', 'ghana', 'togo','united kindom']
 
-# good_countries = ['usa', 'rusia', 'togo']
 
-# bad_countries = []
 
-# for country in countries:
+# # if 'nigeria' in countries:
+# #     print('found')
+
+# #     print('Nigeria is a great country')
+
+# #     c =1 + 7
+
+# #     print(c)
+
+
+
+# #     print('eeeeee')
+# # else:
+# #     print('not found')
+
+
+
+
+# # countries = ['nigeria', 'usa', 'rusia', 'ghana', 'togo','united kindom']
+
+# # good_countries = ['usa', 'rusia', 'togo']
+
+# # bad_countries = []
+
+# # for country in countries:
     
-#     if country not in good_countries:
-#        bad_countries.append(country)
-# print(bad_countries)
+# #     if country not in good_countries:
+# #        bad_countries.append(country)
+# # print(bad_countries)
 
 
 
 
-# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-# newlist = []
+# # fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# # newlist = []
 
-# for fruit in fruits:
-#     newlist.append(fruit)
-# print(newlist)
-
-
+# # for fruit in fruits:
+# #     newlist.append(fruit)
+# # print(newlist)
 
 
 
-# thislist = [100, 50, 65, 82, 23]
-# thislist.sort()
+
+
+# # thislist = [100, 50, 65, 82, 23]
+# # thislist.sort()
+# # # print(thislist)
+
+# # thisset = {"apple", "banana", "cherry"}
+
+# # thisset.add("orange")
+
+# # print(thisset)
+
+
+# # animals= ['dog','cat','rabbit','puppy','chicken', 'snake','lion', 'elephant','monkey','giraffe']
+# # wild_animals= ['lion', 'elephant','monkey','giraffe']
+# # pets = []
+
+
+# # for animal in animals:
+# #     if animal not in wild_animals:
+# #         print(animal)
+
+# # a = " Hello, World!"
+# # print(a.lower())
+# # print(a.upper())
+# # print(a.strip())
+
+# # print(len(a))
+
+
+
+# # a = 1
+# # b = 2
+# # c= 5
+# # y=a**2 + b**2 + c*1
+# # print(y)
+
+# # if a<b:
+# #     print(c/1)
+
+
+# # thislist = ["apple", "banana", "cherry"]
+# # print(thislist[-2])
+
+
+# # thislist = ["apple", "banana", "cherry"]
+# # for x in thislist:
+# #   print(x)
+
+
+
+
+
+
+# # thislist = [100, 50, 65, 82, 23]
+# # thislist.sort()
 # # print(thislist)
 
-# thisset = {"apple", "banana", "cherry"}
 
-# thisset.add("orange")
+# people = [
+#     {
+#         'name' : 'tosin',
+#         'phone': '08037268261',
+#         'age': 32,
+#         'country' : 'nigeria'
+#     },
+#     {
+#         'name' : 'tosin',
+#         'phone': '08037268261',
+#         'age': 33,
+#         'country' : 'usa'
+#     },
+#     {
+#         'name' : 'bukola',
+#         'phone': '08162132121',
+#         'age': 28,
+#         'country' : 'nigeria'
+#     },
+#     {
+#         'name' : 'funke',
+#         'phone': '08032229202',
+#         'age': 34,
+#         'country' : 'nigeria'
+#     },
+#     {
+#         'name' : 'olaolu',
+#         'phone': '08039695719',
+#         'age': 30,
+#         'country' : 'nigeria'
+#     }
+# ]
 
-# print(thisset)
+# for user in people:
 
+ 
 
-# animals= ['dog','cat','rabbit','puppy','chicken', 'snake','lion', 'elephant','monkey','giraffe']
-# wild_animals= ['lion', 'elephant','monkey','giraffe']
-# pets = []
+details = [
+    {
+    'name': 'bukola',
+    'month':'january',
+    'year':1994,
+    'age': 28,
+    'gender':'male'
+    },
+    {
+        'name':'anu',
+        'month':'feb',
+        'year':2000,
+        'age': 20,
+        'gender':'female'
+    },
+    {
+        'name':'tinu',
+        'month':'mar',
+        'year':2040,
+        'age':30,
+        'gender':'male'
 
+    }
+]
+# print(details)
 
-# for animal in animals:
-#     if animal not in wild_animals:
-#         print(animal)
+# user = {
+#     'name':'tinu',
+#     'month':'mar',
+#     'year':2040,
+#     'age':30,
+#     'gender':'male'
 
-# a = " Hello, World!"
-# print(a.lower())
-# print(a.upper())
-# print(a.strip())
+# }
 
-# print(len(a))
-
-
-
-# a = 1
-# b = 2
-# c= 5
-# y=a**2 + b**2 + c*1
-# print(y)
-
-# if a<b:
-#     print(c/1)
-
-
-# thislist = ["apple", "banana", "cherry"]
-# print(thislist[-2])
-
-
-thislist = ["apple", "banana", "cherry"]
-for x in thislist:
-  print(x)
-
-
-
-
-
-
-thislist = [100, 50, 65, 82, 23]
-thislist.sort()
-print(thislist)
-
-
-
-print("tosin")
+# print(user['name'])
 
 
 
 
 
+# for user in details:
+#     print(user['name'])
+
+#     print('my name is' +    user['name']         + "    ueeueueue "  +  user['month'] + "   jddjudj " + user['gender'])
 
 
+# count = 10
+
+# while count > 0:
+
+#     print (count)
+
+#     count = count - 1
+
+# print ('finished')
 
 
+count=20
+while count<30:
+    print(count)
+    count= count + 5
+print('done')
 
 
 
