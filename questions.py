0f3f3ce1b6d04201988eadcfc971b9c3e473582c
Oluwatosin Adesipe <tosin@kobo360.com>
2022-07-08 12:09:59 +0100

    added questions

---
+++ questions.py
@@ -0,0 +1,50 @@
+details = [
+    {
+        'name': 'bukola',
+        'month':'january',
+        'year':1994,
+        'age': 28,
+        'gender':'male',
+        'country' :'nigeria',
+        'salary': 200000
+
+    },
+    {
+        'name':'Anu',
+        'month':'feb',
+        'year':1995,
+        'age': 26,
+        'gender':'female',
+        'country' :'ghana',
+        'salary': 500000
+    },
+    {
+        'name':'Tinu',
+        'month':'mar',
+        'year':1991,
+        'age':30,
+        'gender':'male',
+        'country' :'togo',
+        'salary': 600000
+    },
+    {
+        'name':'Bola',
+        'month':'june',
+        'year':1989,
+        'age':32,
+        'gender':'male',
+        'country' :'togo',
+        'salary': 600000
+    }
+]
+
+
+males = []
+
+females = []
+
+countries = []
+
+Total_salary = 0
+
+highest_paid_person = {}
