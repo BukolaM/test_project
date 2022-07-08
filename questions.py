13918f990e6464b05df0ae5142221c61b512753d
Oluwatosin Adesipe <tosin@kobo360.com>
2022-07-08 13:05:08 +0100

    added more questions

--- questions.py
+++ questions.py
@@ -6,25 +6,28 @@ people = [
         'age': 28,
         'gender':'male',
         'country' :'nigeria',
+        'currency' :'USD',
         'salary': 200000
 
     },
     {
         'name':'Anu',
-        'month':'feb',
+        'month':'febuary',
         'year':1995,
         'age': 26,
         'gender':'female',
         'country' :'ghana',
+        'currency' :'USD',
         'salary': 500000
     },
     {
         'name':'Tinu',
-        'month':'mar',
+        'month':'march',
         'year':1991,
         'age':30,
         'gender':'male',
         'country' :'togo',
+        'currency' :'USD',
         'salary': 1000000
     },
     {
@@ -34,7 +37,48 @@ people = [
         'age':32,
         'gender':'male',
         'country' :'togo',
+        'currency' :'USD',
         'salary': 600000
+    },
+     {
+        'name':'Remi',
+        'month':'june',
+        'year':1986,
+        'age':35,
+        'gender':'female',
+        'country' :'kenya',
+        'currency' :'USD',
+        'salary': 600000
+    },
+     {
+        'name':'Toke',
+        'month':'june',
+        'year':1990,
+        'age':31,
+        'gender':'male',
+        'country' :'nigeria',
+        'currency' :'USD',
+        'salary': 450000
+    },
+    {
+        'name':'Victor',
+        'month':'august',
+        'year':1990,
+        'age':31,
+        'gender':'male',
+        'country' :'togo',
+        'currency' :'USD',
+        'salary': 950000
+    },
+     {
+        'name':'Anu',
+        'month':'january',
+        'year':1989,
+        'age':33,
+        'gender':'male',
+        'country' :'kenya',
+        'currency' :'USD',
+        'salary': 120000
     }
 ]
 
@@ -50,9 +94,20 @@ countries = []
 ## What is the total salay paid
 Total_salary = 0
 
-
 ## Who is the hight paid persin
 highest_paid_person = {}
 
 ## save the uniqu age in this list
 ages = []
+
+## What are the unquie months
+months = []
+
+## What is the total salary paid per country
+salary_per_country = []
+
+## What is the currency of payment
+currency = ''
+
+## which gender make the most money
+hight_paid_gender = ''
